241321deb9bbf43901728227b43df597ca140dd9
Darshan Bhatta <dxb0696@c-smd113-160511.coppellisd.com>
2019-04-16 16:58:45 -0500

    begin login screen code

--- app/src/main/java/ml/coppellcoders/coolmathgame/LoginScreen.java
+++ app/src/main/java/ml/coppellcoders/coolmathgame/LoginScreen.java
@@ -1,13 +1,71 @@
 package ml.coppellcoders.coolmathgame;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreen extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static String TAG = "LoginScreen.java";
+    private EditText email, password;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!email.getText().toString().isEmpty() && !password.getText().toString().isEmpty()){
+                    mAuth.signInWithEmailAndPassword(email.getText().toString().trim(),
+                            password.getText().toString().trim()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "One or fields are empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
     }
 }
